c21b9129158b5028554084209d55279f97d7b67e
Redeye424 <shawngoldberg424@gmail.com>
2026-03-08 22:38:53 -0400

    Update admin.py

--- firstapp/admin.py
+++ firstapp/admin.py
@@ -1,9 +1,3 @@
 from django.contrib import admin
 from django import forms
 from django.contrib.auth.models import User
-from .models import shoe_todo, Profile, Notification
-
-
-admin.site.register(shoe_todo)
-admin.site.register(Profile)
-admin.site.register(Notification)
