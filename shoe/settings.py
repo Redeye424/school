49a54d8dbbe6ae3b6d49be31f2bb817758642433
Redeye424 <shawngoldberg424@gmail.com>
2026-04-30 18:11:06 -0400

    Update settings.py

--- shoe/settings.py
+++ shoe/settings.py
@@ -20,12 +20,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-rd!93-l@t+u+@8(kpkrb(-5n#*u%vazuef!psrn9(j=9x10%#('
+SECRET_KEY = 'django-insecure-rd!93-l@t+u+@8(kprkb(-5n#*u%vabuef!psrn9(j=9x10%#('
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ["homework42.pythonanywhere.com"]
+ALLOWED_HOSTS = ["USERNAME-HERE.pythonanywhere.com"]
 
 X_FRAME_OPTIONS = "SAMEORIGIN"
 # Application definition
